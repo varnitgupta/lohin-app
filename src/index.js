77f7d3243bc3a20818362f07e6a6201026007f96
varnitgupta <varnit9876@gmail.com>
2023-10-17 19:25:11 +0530

    sec

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Root from "./routes/root"
+import ErrorPage from "./Error-page";
+import Contact from "./routes/contact";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    children:[
+      {
+        path: "/contacts/:contactId",
+        element: <Contact/>
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
